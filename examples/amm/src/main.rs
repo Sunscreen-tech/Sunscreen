ce952f4d40c5fed3063060606195f0caa4661aab
ravital <47154865+ravital@users.noreply.github.com>
2022-03-24 09:57:23 -0700

    Update main.rs

--- examples/amm/src/main.rs
+++ examples/amm/src/main.rs
@@ -14,6 +14,7 @@ use sunscreen::{
  * This program swaps NU tokens to receive ETH.
  */
 fn swap_nu(trade_nu: Cipher<Rational>, total_eth: Rational, total_nu: Rational, total_tokens: Rational) -> Cipher<Rational> {
+    // RS: I would call trade_nu something else. I can see it as being confusing having "trade_nu" and "swap_nu"
     // total_tokens should equal total_eth * total_nu. Sunscreen doesn't
     // currently support plaintext-plaintext multiply, so this needs to
     // be computed outside of FHE.
@@ -21,6 +22,7 @@ fn swap_nu(trade_nu: Cipher<Rational>, total_eth: Rational, total_nu: Rational, 
 }
 
 struct Miner {
+    // RS: I would probably clarify what swap_fhe is (i.e. the compiled swap_nu program)
     pub swap_fhe: CompiledFheProgram,
     pub total_eth: f64,
     pub total_nu: f64,
@@ -41,7 +43,8 @@ impl Miner {
 
     pub fn run_contract(&self, trade_nu: Ciphertext, public_key: &PublicKey) -> Ciphertext {
         let runtime = Runtime::new(&self.swap_fhe.metadata.params).unwrap();
-
+        
+        // RS: will need to clarify what's going on here...
         let args: Vec<FheProgramInput> = vec![
             trade_nu.into(),
             Rational::try_from(self.total_eth).unwrap().into(),
@@ -59,12 +62,17 @@ fn main() {
     let miner = Miner::setup(100.0, 1000.0);
 
     let runtime = Runtime::new(&miner.swap_fhe.metadata.params).unwrap();
+    
+    // RS: whose public/private key is this? the reader does not know. no division of parties here.
     let (public_key, private_key) = runtime.generate_keys().unwrap();
 
+    // RS: would call trade_nu something else...
     let trade_nu = runtime.encrypt(Rational::try_from(20.0).unwrap(), &public_key).unwrap();
 
+    // RS: would prefer if this had a different name so it's clear to the reader the amount is encrypted
     let received_eth = miner.run_contract(trade_nu, &public_key);
 
+    // RS: unclear who is doing this? need the division of parties or just to omit it.
     let received_eth: Rational = runtime.decrypt(&received_eth, &private_key).unwrap();
 
     let received_eth: f64 = received_eth.into();

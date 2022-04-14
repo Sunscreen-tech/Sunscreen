fe23df89d2fdbaed5182913a8909e4c8f8dc6e9e
Rick Weber <rick@sunscreen.tech>
2022-04-14 16:39:05 -0700

    hexl feature

--- seal/build.rs
+++ seal/build.rs
@@ -6,7 +6,7 @@ use cmake::Config;
 use std::path::{Path, PathBuf};
 
 fn compile_native(profile: &str, out_path: &Path) {
-    let hexl = if std::env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64" {
+    let hexl = if std::env::var("CARGO_FEATURE_HEXL").is_ok() {
         "ON"
     } else {
         "OFF"

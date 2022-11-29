b1434a2fc0790965d65fcde4746de9e8748917eb
rickwebiii <rick.weber.iii@gmail.com>
2022-11-29 11:38:53 -0800

    Impl Fmt for error

--- sunscreen_fhe_program/src/error.rs
+++ sunscreen_fhe_program/src/error.rs
@@ -8,7 +8,7 @@ use crate::{EdgeInfo, OutputType};
  */
 pub type OpName = String;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
 /**
  * Represents an error that can occur in this crate.
  */
@@ -17,11 +17,13 @@ pub enum Error {
      * The given [`FheProgram`](crate::FheProgram) has
      * one or more errors. The inner value is the list of errors.
      */
+    #[error("The following errors occurred: {0:#?}")]
     IRError(Box<Vec<IRError>>),
 
     /**
      * Attempted to deserialize and unknown scheme type.
      */
+    #[error("Attempted to deserialize and unknown scheme type.")]
     InvalidSchemeType,
 }
 
@@ -39,7 +41,7 @@ impl Error {
 /**
  * An error in an [`FheProgram`](crate::FheProgram).
  */
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
 pub enum IRError {
     /**
      * The IR has a cycle.
@@ -50,6 +52,17 @@ pub enum IRError {
      * A node in the IR has an error.
      */
     NodeError(Box<(NodeIndex, OpName, NodeError)>),
+}
+
+impl std::fmt::Display for IRError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IRHasCycles => { write!(f, "This FHE program has one or more cycles") },
+            Self::NodeError(x) => {
+                write!(f, "Node {}:{} encountered an error: {}", x.1, x.0.index(), x.2)
+            }
+        }
+    }
 }
 
 impl IRError {
@@ -66,7 +79,7 @@ const_assert!(std::mem::size_of::<IRError>() <= 16);
 /**
  * An error on a node in an [`FheProgram`](crate::FheProgram).
  */
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
 pub enum NodeError {
     /**
      * The node is missing an expected operand of the contained type.
@@ -90,6 +103,25 @@ pub enum NodeError {
      * but got some other number (second argument).
      */
     WrongOperandCount(Box<(usize, usize)>),
+}
+
+impl std::fmt::Display for NodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::MissingOperand(e) => {
+                write!(f, "This node is missing operand of type {e:#?}")
+            },
+            Self::MissingParent(idx) => {
+                write!(f, "This node is missing a parent at index {}", idx.index())
+            },
+            Self::ParentHasIncorrectOutputType(x) => {
+                write!(f, "For the {:#?} operand, expected an output of {:#?} but found {:#?}.", x.0, x.1, x.2)
+            },
+            Self::WrongOperandCount(x) => {
+                write!(f, "Incorrect operand count. Expected {}. Found {}.", x.0, x.1)
+            }
+        }
+    }
 }
 
 impl NodeError {

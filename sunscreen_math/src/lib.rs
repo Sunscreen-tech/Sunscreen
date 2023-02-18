1f582d03f9fca163700ed4eba89d806f006b94f0
rickwebiii <rick.weber.iii@gmail.com>
2023-02-17 23:19:21 -0800

    Add hybrid computation vector

--- sunscreen_math/src/lib.rs
+++ sunscreen_math/src/lib.rs
@@ -6,6 +6,11 @@ mod metal_impl;
 mod cpu;
 pub use cpu::{CpuScalarVec, CpuRistrettoPointVec};
 
+#[cfg(feature = "pina")]
+mod pina;
+#[cfg(feature = "pina")]
+pub use pina::{PinaScalarVec, PinaRistrettoPointVec};
+
 #[cfg(all(feature = "pina", not(feature = "gpu")))]
 compile_error!("feature pina requires a GPU backend feature.");
 
@@ -18,13 +23,19 @@ pub use metal_impl::GpuScalarVec;
 #[cfg(feature = "metal")]
 pub use metal_impl::GpuVec;
 
-#[cfg(feature = "gpu")]
+#[cfg(feature = "pina")]
+pub type RistrettoPointVec = PinaRistrettoPointVec;
+
+#[cfg(all(feature = "gpu", not(feature = "pina")))]
 pub type RistrettoPointVec = GpuRistrettoPointVec;
 
 #[cfg(not(feature = "gpu"))]
 pub type RistrettoPointVec = CpuRistrettoPointVec;
 
-#[cfg(feature = "gpu")]
+#[cfg(feature = "pina")]
+pub type ScalarVec = PinaScalarVec;
+
+#[cfg(all(feature = "gpu", not(feature = "pina")))]
 pub type ScalarVec = GpuScalarVec;
 
 #[cfg(not(feature = "gpu"))]

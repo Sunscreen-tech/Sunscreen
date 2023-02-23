c47c1884bebd59f67f479a5735006ada4d2107f1
rickwebiii <rick.weber.iii@gmail.com>
2023-02-22 17:35:15 -0800

    WIP

--- sunscreen_math/src/webgpu_impl/mod.rs
+++ sunscreen_math/src/webgpu_impl/mod.rs
@@ -1,8 +1,11 @@
-use std::borrow::Cow;
+use core::slice;
+use std::{borrow::Cow, ops::Deref, mem::{align_of, MaybeUninit, size_of}};
 
+use bytemuck::{cast, cast_slice, Pod};
+use futures::channel::oneshot;
 use lazy_static::lazy_static;
 use tokio::runtime::{Builder as TokioRuntimeBuilder, Runtime as TokioRuntime};
-use wgpu::{Instance, RequestAdapterOptions, Device, Queue, ShaderModuleDescriptor, ShaderModule};
+use wgpu::{Instance, RequestAdapterOptions, Device, Queue, ShaderModuleDescriptor, ShaderModule, BufferDescriptor, COPY_BUFFER_ALIGNMENT, BufferUsages, Buffer, ComputePipelineDescriptor, BindGroupDescriptor, BindGroupEntry, CommandEncoderDescriptor, ComputePassDescriptor, util::{BufferInitDescriptor, DeviceExt}, Maintain};
 
 mod ristrettovec;
 mod scalarvec;
@@ -16,9 +19,134 @@ pub struct Runtime {
     shaders: ShaderModule,
 }
 
+fn assert_aligned<T>(ptr: *const T) {
+    assert!(ptr.cast::<()>().align_offset(align_of::<T>()) == 0);
+}
+
+pub struct Grid((u32, u32, u32));
+
+impl Grid {
+    pub fn new(x: u32, y: u32, z: u32) -> Self {
+        Self((x, y, z))
+    }
+}
+
+trait BufferExt {
+    fn get_data<T: Pod + Copy>(&self) -> Vec<T>;
+}
+
+impl BufferExt for Buffer {
+    fn get_data<T: Pod + Copy>(&self) -> Vec<T> {
+        let (s, r) = oneshot::channel();
+
+        // In vanilla WebGPU, if you use the MAP_READ flag, you must also set COPY_DST
+        // and *only* COPY_DST. This means you can't use such buffers in compute 
+        // shaders. As such, we create a temporary buffer with these properties so we
+        // can copy data out of the shader-capable buffer and return the results.
+        let runtime = Runtime::get();
+
+        let copy_buf = runtime.device.create_buffer(&BufferDescriptor {
+            label: None,
+            size: self.size(),
+            usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
+            mapped_at_creation: false
+        });
+
+        let mut encoder = runtime.device.create_command_encoder(&CommandEncoderDescriptor { label: None });
+        encoder.copy_buffer_to_buffer(self, 0, &copy_buf, 0, self.size());
+
+        runtime.queue.submit(Some(encoder.finish()));
+
+        let buffer_slice = copy_buf.slice(..);
+        buffer_slice.map_async(wgpu::MapMode::Read, move |v| { s.send(v).unwrap(); });
+
+        runtime.device.poll(Maintain::Wait);
+
+        TOKIO_RUNTIME.block_on(async {
+            r.await.unwrap().unwrap();
+
+            let s = buffer_slice.get_mapped_range();
+            bytemuck::cast_slice(&s).to_owned()
+        })
+    }
+}
+
 impl Runtime {
     pub fn get() -> &'static Self {
         &RUNTIME
+    }
+
+    /// Allocates space for `len` elements of type `T`.
+    pub fn alloc<T>(&self, len: usize) -> Buffer {
+        let len = size_of::<T>() * len;
+
+        // Round up len to a multiple of COPY_BUFFER_ALIGNMENT, as required to use 
+        // mapped_at_creation=true
+        let len = if len % COPY_BUFFER_ALIGNMENT as usize == 0 { len } else { (len / COPY_BUFFER_ALIGNMENT as usize + 1) * COPY_BUFFER_ALIGNMENT as usize};
+
+        let buffer = self.device.create_buffer(&BufferDescriptor {
+            label: None,
+            size: len as u64,
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
+            mapped_at_creation: false
+        });
+
+        buffer
+    }
+
+    pub fn alloc_from_slice<T: Pod>(&self, data: &[T]) -> Buffer {
+        let len = size_of::<T>() * data.len();
+
+        // Round up len to a multiple of COPY_BUFFER_ALIGNMENT, as required to use 
+        // mapped_at_creation=true
+        let len = if len % COPY_BUFFER_ALIGNMENT as usize == 0 { len } else { (len / COPY_BUFFER_ALIGNMENT as usize + 1) * COPY_BUFFER_ALIGNMENT as usize};
+
+        let buffer = self.device.create_buffer_init(&BufferInitDescriptor {
+            label: None,
+            contents: cast_slice(data),
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
+        });
+
+        buffer
+    }
+
+    pub fn run(&self, kernel_name: &'static str, args: &[&Buffer], threadgroups: &Grid) {
+        let pipeline = self.device.create_compute_pipeline(&ComputePipelineDescriptor {
+            label: None,
+            layout: None,
+            module: &self.shaders,
+            entry_point: kernel_name
+        });
+
+        let bindings = args.iter().enumerate().map(|(i, b)| {
+            BindGroupEntry {
+                binding: i as u32,
+                resource: b.as_entire_binding()
+            }
+        }).collect::<Vec<_>>();
+
+        let layout = pipeline.get_bind_group_layout(0);
+
+        let bind_group = self.device.create_bind_group(&BindGroupDescriptor {
+            label: None,
+            layout: &layout,
+            entries: &bindings,
+        });
+
+        let mut encoder = self.device.create_command_encoder(&CommandEncoderDescriptor { label: None });
+
+        {
+            let (x, y, z) = threadgroups.0;
+
+            let mut pass = encoder.begin_compute_pass(&ComputePassDescriptor { label: None });
+            pass.set_pipeline(&pipeline);
+            pass.set_bind_group(0, &bind_group, &[]);
+            pass.dispatch_workgroups(x, y, z);
+        }
+
+        self.queue.submit(Some(encoder.finish()));
+
+        assert!(self.device.poll(wgpu::MaintainBase::Wait));
     }
 }
 
@@ -69,12 +197,47 @@ lazy_static! {
     };
 }
 
+pub struct GpuU32 {
+    data: Buffer
+}
+
+impl GpuU32 {
+    pub fn new(val: u32) -> Self {
+        let runtime = Runtime::get();
+        let data = runtime.alloc_from_slice(&[val]);
+
+        Self {
+            data
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::mem::size_of;
+
     use super::*;
 
     #[test]
     fn can_get_runtime() {
         let runtime = Runtime::get();
+    }
+
+    #[test]
+    fn can_add_vectors() {
+        let runtime = Runtime::get();
+
+        let a = [1u32, 2, 3, 4];
+        let b = [4u32, 5, 6, 7];
+
+        let a_gpu = runtime.alloc_from_slice(&a);
+        let b_gpu = runtime.alloc_from_slice(&b);
+        let c_gpu = runtime.alloc::<u32>(a.len());
+
+        runtime.run("add", &[&a_gpu, &b_gpu, &c_gpu], &Grid::new(1, 1, 1));
+
+        for (c, (a, b)) in c_gpu.get_data::<u32>().iter().zip(a.iter().zip(b.iter())) {
+            assert_eq!(*c, a + b);
+        }
     }
 }
--- sunscreen_math/src/webgpu_impl/shaders/hello.wgsl
+++ sunscreen_math/src/webgpu_impl/shaders/hello.wgsl
@@ -1,8 +1,11 @@
-@compute
-@workgroup_size(128)
-fn main(
-    @builtin(global_invocation_id) global_id: vec3<u32>
+@group(0) @binding(0) var<storage, read> a : array<u32>;
+@group(0) @binding(1) var<storage, read> b : array<u32>;
+@group(0) @binding(2) var<storage, read_write> c : array<u32>;
 
+@compute
+@workgroup_size(128, 1, 1)
+fn add(
+    @builtin(global_invocation_id) gid: vec3<u32>,
 ) {
-    
+    c[gid.x] = a[gid.x] + b[gid.x];
 }

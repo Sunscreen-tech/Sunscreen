90b2b6ae172a9993ba06a792c1e0506123780f87
rickwebiii <rick.weber.iii@gmail.com>
2023-03-02 15:51:55 -0800

    Add chi squared and mean-var proofs

--- sunscreen/benches/fractional_range_proof.rs
+++ sunscreen/benches/fractional_range_proof.rs
@@ -253,5 +253,122 @@ fn mean_variance_fractional_range_proof(_c: &mut Criterion) {
     println!("Verifier time {}s", verifier_time.elapsed().as_secs_f64());
 }
 
-criterion_group!(benches, private_tx_fractional_range_proof, mean_variance_fractional_range_proof);
+/// Suppose we're adding ZKPs on the inputs to our chi squared example (see
+/// examples/chi_sq/main.rs). We want to prove our ciphertexts are well-formed and
+/// that the contained [`Signed`](sunscreen::types::bfv::Signed) messages are between 
+/// 0 and 12.
+/// 
+/// This proof is similar to [`mean_variance_fractional_range_proof`] except we're
+/// proving 3 values are between 0 <= a_i <= 12 where
+/// 
+/// * a_0, a_1, a_2 are submitted value under a given user's key.
+fn chi_sq_fractional_range_proof(_c: &mut Criterion) {
+    #[zkp_program(backend = "bulletproofs")]
+    /**
+     * Proves the 0 < a <= b and a == c
+     */
+    fn in_range<F: BackendField>(
+        a_0: [[NativeField<F>; 8]; 64],
+        a_1: [[NativeField<F>; 8]; 64],
+        a_2: [[NativeField<F>; 8]; 64],
+    ) {
+        println!("Running chi_sq_fractional_range_proof...");
+
+        let get_coeffs = |x: &[[ProgramNode<NativeField<F>>; 8]]| -> Vec<_> {
+            x.iter().map(|x| to_field_element(x, true)).collect()
+        };
+
+        let a_0_coeffs = get_coeffs(&a_0);
+        let a_1_coeffs = get_coeffs(&a_1);
+        let a_2_coeffs = get_coeffs(&a_2);
+
+        let a_0_val = to_field_element(&a_0_coeffs, false);
+        let a_1_val = to_field_element(&a_1_coeffs, false);
+        let a_2_val = to_field_element(&a_2_coeffs, false);
+
+        let zero = NativeField::<F>::from(0).into_program_node();
+        let twelve = NativeField::<F>::from(12).into_program_node();
+
+        a_0_val.constrain_ge_bounded(zero, 8);
+        a_0_val.constrain_le_bounded(twelve, 8);
+        a_1_val.constrain_ge_bounded(zero, 8);
+        a_1_val.constrain_le_bounded(twelve, 8);
+        a_2_val.constrain_ge_bounded(zero, 8);
+        a_2_val.constrain_le_bounded(twelve, 8);
+    }
+
+    let app = Compiler::new()
+        .zkp_backend::<BulletproofsBackend>()
+        .zkp_program(in_range)
+        .compile()
+        .unwrap();
+
+    let prog = app.get_zkp_program(in_range).unwrap();
+
+    fn encode(val: i8) -> [NativeField<BPField>; 8] {
+        let as_u8 = val.to_le_bytes()[0];
+
+        (0..8)
+            .map(|x| NativeField::from((as_u8 >> x) & 0x1))
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
+    }
+
+    // Create a carryless binary value.
+    // a is 3 in the 1s place, 2 in the 2s place.
+    // 3 * 1 + 2 * 2 = 7
+    let mut a_0 = vec![0i8; 64];
+    a_0[0] = 3;
+    a_0[1] = 2;
+
+    let a_0: [[NativeField<BPField>; 8]; 64] = a_0
+        .iter()
+        .map(|x| encode(*x))
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    // 4 * 1 + 1 * 2 = 8
+    let mut a_1 = vec![0i8; 64];
+    a_1[0] = 4;
+    a_1[1] = 1;
+
+    let a_1: [[NativeField<BPField>; 8]; 64] = a_1
+        .iter()
+        .map(|x| encode(*x))
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    // 3 * 1 + 2 * 2 = 11
+    let mut a_2 = vec![0i8; 64];
+    a_2[0] = 3;
+    a_2[1] = 2;
+
+    let a_2: [[NativeField<BPField>; 8]; 64] = a_2
+        .iter()
+        .map(|x| encode(*x))
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    let runtime = Runtime::new_zkp(&BulletproofsBackend::new()).unwrap();
+
+    let prover_time = Instant::now();
+
+    let proof = runtime.prove(prog, vec![], vec![], vec![a_0, a_1, a_2]).unwrap();
+
+    println!("Prover time {}s", prover_time.elapsed().as_secs_f64());
+
+    let verifier_time = Instant::now();
+
+    runtime
+        .verify(prog, &proof, Vec::<ZkpProgramInput>::new(), vec![])
+        .unwrap();
+
+    println!("Verifier time {}s", verifier_time.elapsed().as_secs_f64());
+}
+
+criterion_group!(benches, private_tx_fractional_range_proof, mean_variance_fractional_range_proof, chi_sq_fractional_range_proof);
 criterion_main!(benches);

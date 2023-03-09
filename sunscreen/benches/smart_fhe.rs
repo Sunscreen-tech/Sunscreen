c5767fd3aa856f1ae7f402cf32b92a5149273322
rickwebiii <rick.weber.iii@gmail.com>
2023-03-08 17:11:38 -0800

    Average values

--- sunscreen/benches/smart_fhe.rs
+++ sunscreen/benches/smart_fhe.rs
@@ -8,6 +8,11 @@ use sunscreen::{Params, fhe_program, types::{bfv::Fractional, Cipher}, Compiler,
 fn benchmark(params: &Params) {
     println!("Benchmarking for {:#?}", params);
 
+    let mut compile_time = 0.0;
+    let mut encrypt_time = 0.0;
+    let mut run_time = 0.0;
+    let mut keygen_time = 0.0;
+
     #[fhe_program(scheme = "bfv")]
     fn add(a: Cipher<Fractional<32>>, b: Cipher<Fractional<32>>) -> Cipher<Fractional<32>> {
         a + b
@@ -18,49 +23,59 @@ fn benchmark(params: &Params) {
         a - b
     }
 
-    let now = Instant::now();
-    let app = Compiler::new()
-        .fhe_program(add)
-        .fhe_program(sub)
-        .with_params(&params)
-        .compile()
-        .unwrap();
+    const RUNS: u32 = 100;
 
-    println!("Compilation time {}s", now.elapsed().as_secs_f64());
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        let app = Compiler::new()
+            .fhe_program(add)
+            .fhe_program(sub)
+            .with_params(&params)
+            .compile()
+            .unwrap();
 
-    let runtime = Runtime::new_fhe(params).unwrap();
+        compile_time += now.elapsed().as_secs_f64();
 
-    let now = Instant::now();
-    let (public, private) = runtime.generate_keys().unwrap();
+        let runtime = Runtime::new_fhe(params).unwrap();
 
-    println!("Keygen time: {}s", now.elapsed().as_secs_f64());
+        let now = Instant::now();
+        let (public, private) = runtime.generate_keys().unwrap();
 
-    let now = Instant::now();
-    
-    // Encrypt under Alice's key
-    let send_a = runtime.encrypt(Fractional::<32>::from(42.0), &public).unwrap();
-    // Encrypt under Bob's key, but we'll pretend Alice's key is Bob's.
-    // We just want the timings.
-    let send_b = runtime.encrypt(Fractional::<32>::from(42.0), &public).unwrap();
+        keygen_time += now.elapsed().as_secs_f64();
 
-    println!("Encrypt time: {}s", now.elapsed().as_secs_f64());
+        let now = Instant::now();
+        
+        // Encrypt under Alice's key
+        let send_a = runtime.encrypt(Fractional::<32>::from(42.0), &public).unwrap();
+        // Encrypt under Bob's key, but we'll pretend Alice's key is Bob's.
+        // We just want the timings.
+        let send_b = runtime.encrypt(Fractional::<32>::from(42.0), &public).unwrap();
 
-    let now = Instant::now();
-    
-    let add_res = runtime.run(app.get_fhe_program(add).unwrap(), vec![send_a.clone(), send_b.clone()], &public).unwrap();
-    let sub_res = runtime.run(app.get_fhe_program(sub).unwrap(), vec![send_a, send_b], &public).unwrap();
+        encrypt_time += now.elapsed().as_secs_f64();
 
-    println!("Run FHE circuit time {}s", now.elapsed().as_secs_f64());
+        let now = Instant::now();
+        
+        let add_res = runtime.run(app.get_fhe_program(add).unwrap(), vec![send_a.clone(), send_b.clone()], &public).unwrap();
+        let sub_res = runtime.run(app.get_fhe_program(sub).unwrap(), vec![send_a, send_b], &public).unwrap();
 
-    let add_res: Fractional<32> = runtime.decrypt(&add_res[0], &private).unwrap();
-    let add_res: f64 = add_res.into();
+        run_time += now.elapsed().as_secs_f64();
 
-    assert_eq!(add_res, 84.0);
+        let add_res: Fractional<32> = runtime.decrypt(&add_res[0], &private).unwrap();
+        let add_res: f64 = add_res.into();
 
-    let sub_res: Fractional<32> = runtime.decrypt(&sub_res[0], &private).unwrap();
-    let sub_res: f64 = sub_res.into();
+        assert_eq!(add_res, 84.0);
 
-    assert_eq!(sub_res, 0.0);
+        let sub_res: Fractional<32> = runtime.decrypt(&sub_res[0], &private).unwrap();
+        let sub_res: f64 = sub_res.into();
+
+        assert_eq!(sub_res, 0.0);
+    }
+
+    println!("Compilation time {}s", compile_time / RUNS as f64);
+    println!("Keygen time: {}s", keygen_time / RUNS as f64);
+    println!("Encrypt time: {}s", encrypt_time / RUNS as f64);
+    println!("Run FHE circuit time {}s", run_time / RUNS as f64);
+
 
 }
 

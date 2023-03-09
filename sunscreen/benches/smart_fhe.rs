af4849ddb5925fdab6efc7a99a223ec6be42eebb
rickwebiii <rick.weber.iii@gmail.com>
2023-03-08 19:19:59 -0800

    Time shield circuits (#224)
    * Time shield/unshield circuits

--- sunscreen/benches/smart_fhe.rs
+++ sunscreen/benches/smart_fhe.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 use sunscreen::{
     fhe_program,
     types::{bfv::Fractional, Cipher},
-    Compiler, Params, Runtime, SchemeType,
+    Compiler, FheProgramInput, Params, Runtime, SchemeType,
 };
 
 /// This program benchmarks all the FHE pieces of private transactions with
@@ -16,6 +16,8 @@ fn benchmark(params: &Params) {
     let mut encrypt_time = 0.0;
     let mut run_time = 0.0;
     let mut keygen_time = 0.0;
+    let mut shield = 0.0;
+    let mut unshield = 0.0;
 
     #[fhe_program(scheme = "bfv")]
     fn add(a: Cipher<Fractional<32>>, b: Cipher<Fractional<32>>) -> Cipher<Fractional<32>> {
@@ -27,13 +29,26 @@ fn benchmark(params: &Params) {
         a - b
     }
 
+    #[fhe_program(scheme = "bfv")]
+    fn add_pt(a: Cipher<Fractional<32>>, b: Fractional<32>) -> Cipher<Fractional<32>> {
+        a + b
+    }
+
+    #[fhe_program(scheme = "bfv")]
+    fn sub_pt(a: Cipher<Fractional<32>>, b: Fractional<32>) -> Cipher<Fractional<32>> {
+        a - b
+    }
+
     const RUNS: u32 = 100;
+    let mut ct_size = 0usize;
 
     for _ in 0..RUNS {
         let now = Instant::now();
         let app = Compiler::new()
             .fhe_program(add)
             .fhe_program(sub)
+            .fhe_program(add_pt)
+            .fhe_program(sub_pt)
             .with_params(params)
             .compile()
             .unwrap();
@@ -73,7 +88,7 @@ fn benchmark(params: &Params) {
         let sub_res = runtime
             .run(
                 app.get_fhe_program(sub).unwrap(),
-                vec![send_a, send_b],
+                vec![send_a.clone(), send_b],
                 &public,
             )
             .unwrap();
@@ -89,12 +104,52 @@ fn benchmark(params: &Params) {
         let sub_res: f64 = sub_res.into();
 
         assert_eq!(sub_res, 0.0);
+
+        // Benchmark shielded TX
+        let args: Vec<FheProgramInput> =
+            vec![send_a.clone().into(), Fractional::<32>::from(42.0).into()];
+
+        let now = Instant::now();
+
+        let add_res = runtime
+            .run(app.get_fhe_program(add_pt).unwrap(), args, &public)
+            .unwrap();
+
+        shield += now.elapsed().as_secs_f64();
+
+        let add_res: Fractional<32> = runtime.decrypt(&add_res[0], &private).unwrap();
+        let add_res: f64 = add_res.into();
+
+        assert_eq!(add_res, 84.0);
+
+        // Benchmark unshield
+        let args: Vec<FheProgramInput> =
+            vec![send_a.clone().into(), Fractional::<32>::from(42.0).into()];
+
+        let now = Instant::now();
+
+        let sub_res = runtime
+            .run(app.get_fhe_program(sub_pt).unwrap(), args, &public)
+            .unwrap();
+
+        unshield += now.elapsed().as_secs_f64();
+
+        let sub_res: Fractional<32> = runtime.decrypt(&sub_res[0], &private).unwrap();
+        let sub_res: f64 = sub_res.into();
+
+        assert_eq!(sub_res, 0.0);
+
+        let ser = bincode::serialize(&send_a).unwrap();
+        ct_size = ser.len();
     }
 
     println!("Compilation time {}s", compile_time / RUNS as f64);
     println!("Keygen time: {}s", keygen_time / RUNS as f64);
     println!("Encrypt time: {}s", encrypt_time / RUNS as f64);
     println!("Run FHE circuit time {}s", run_time / RUNS as f64);
+    println!("Shield FHE circuit time {}s", shield / RUNS as f64);
+    println!("Unshield FHE circuit time {}s", unshield / RUNS as f64);
+    println!("CT size {}", ct_size);
 }
 
 //262,152

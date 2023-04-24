ace6d97140b6366613464c32693ac2e9bebefa5c
Sam Tay <samctay@pm.me>
2023-04-24 11:57:33 -0700

    Add a `bfv::Unsigned` type (#236)
    * Add a bfv::Unsigned type
    
    * Fix decoding from polynomial?
    
    * Add prop test for addition
    
    * Proptest sub, mul, and mixed cipher/plaintexts
    
    * Reduce code dupe
    
    * Add test for 0 - 1
    
    * Add a few more tests

---
+++ sunscreen/src/types/bfv/unsigned.rs
@@ -0,0 +1,356 @@
+use std::ops::*;
+
+use crypto_bigint::{UInt, Wrapping};
+use paste::paste;
+use seal_fhe::Plaintext as SealPlaintext;
+
+use sunscreen_runtime::{
+    InnerPlaintext, NumCiphertexts, Plaintext, TryFromPlaintext, TryIntoPlaintext,
+};
+
+use crate as sunscreen;
+use crate::{
+    fhe::{with_fhe_ctx, FheContextOps},
+    types::{
+        ops::{
+            GraphCipherAdd, GraphCipherConstAdd, GraphCipherConstMul, GraphCipherConstSub,
+            GraphCipherMul, GraphCipherPlainAdd, GraphCipherPlainMul, GraphCipherPlainSub,
+            GraphCipherSub, GraphConstCipherSub, GraphPlainCipherSub,
+        },
+        Cipher,
+    },
+};
+use crate::{
+    types::{intern::FheProgramNode, BfvType, FheType, TypeNameInstance},
+    FheProgramInputTrait, Params, TypeName as DeriveTypeName, WithContext,
+};
+
+/// Unsigned 64 bit integer
+pub type Unsigned64 = Unsigned<1>;
+/// Unsigned 128 bit integer
+pub type Unsigned128 = Unsigned<2>;
+/// Unsigned 256 bit integer
+pub type Unsigned256 = Unsigned<4>;
+
+#[derive(Debug, Clone, Copy, DeriveTypeName, PartialEq, Eq)]
+/**
+ * A single unsigned integer.
+ */
+pub struct Unsigned<const LIMBS: usize> {
+    val: UInt<LIMBS>,
+}
+
+impl<const LIMBS: usize> NumCiphertexts for Unsigned<LIMBS> {
+    const NUM_CIPHERTEXTS: usize = 1;
+}
+
+impl<const LIMBS: usize> FheProgramInputTrait for Unsigned<LIMBS> {}
+impl<const LIMBS: usize> FheType for Unsigned<LIMBS> {}
+impl<const LIMBS: usize> BfvType for Unsigned<LIMBS> {}
+
+impl<const LIMBS: usize> std::fmt::Display for Unsigned<LIMBS> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.val)
+    }
+}
+
+impl<const LIMBS: usize> Default for Unsigned<LIMBS> {
+    fn default() -> Self {
+        Self::from(UInt::ZERO)
+    }
+}
+
+impl<const LIMBS: usize> TryIntoPlaintext for Unsigned<LIMBS> {
+    fn try_into_plaintext(
+        &self,
+        params: &Params,
+    ) -> std::result::Result<Plaintext, sunscreen_runtime::Error> {
+        let mut seal_plaintext = SealPlaintext::new()?;
+
+        let sig_bits = self.val.bits_vartime();
+        seal_plaintext.resize(sig_bits);
+
+        for i in 0..sig_bits {
+            let bit_value = self.val.bit_vartime(i);
+            seal_plaintext.set_coefficient(i, bit_value);
+        }
+
+        Ok(Plaintext {
+            data_type: self.type_name_instance(),
+            inner: InnerPlaintext::Seal(vec![WithContext {
+                params: params.clone(),
+                data: seal_plaintext,
+            }]),
+        })
+    }
+}
+
+impl<const LIMBS: usize> TryFromPlaintext for Unsigned<LIMBS> {
+    fn try_from_plaintext(
+        plaintext: &Plaintext,
+        params: &Params,
+    ) -> std::result::Result<Self, sunscreen_runtime::Error> {
+        let val = match &plaintext.inner {
+            InnerPlaintext::Seal(p) => {
+                if p.len() != 1 {
+                    return Err(sunscreen_runtime::Error::IncorrectCiphertextCount);
+                }
+
+                let bits = usize::min(std::mem::size_of::<UInt<LIMBS>>() * 8, p[0].len());
+
+                let negative_cutoff = (params.plain_modulus + 1) / 2;
+
+                let mut val = UInt::ZERO;
+                for i in 0..bits {
+                    let coeff = p[0].get_coefficient(i);
+                    if coeff < negative_cutoff {
+                        val = wrapping_add(
+                            val,
+                            wrapping_mul(UInt::from_u8(0x1) << i, UInt::from_u64(coeff)),
+                        );
+                    } else {
+                        val = wrapping_sub(
+                            val,
+                            wrapping_mul(
+                                UInt::from_u8(0x1) << i,
+                                UInt::from_u64(params.plain_modulus - coeff),
+                            ),
+                        );
+                    }
+                }
+
+                Self { val }
+            }
+        };
+
+        Ok(val)
+    }
+}
+
+impl<const LIMBS: usize> From<UInt<LIMBS>> for Unsigned<LIMBS> {
+    fn from(val: UInt<LIMBS>) -> Self {
+        Self { val }
+    }
+}
+
+impl<const LIMBS: usize> From<u64> for Unsigned<LIMBS> {
+    fn from(n: u64) -> Self {
+        Self {
+            val: UInt::from_u64(n),
+        }
+    }
+}
+
+impl<const LIMBS: usize> From<Unsigned<LIMBS>> for UInt<LIMBS> {
+    fn from(unsigned: Unsigned<LIMBS>) -> Self {
+        unsigned.val
+    }
+}
+
+fn wrapping_add<const LIMBS: usize>(lhs: UInt<LIMBS>, rhs: UInt<LIMBS>) -> UInt<LIMBS> {
+    (Wrapping(lhs) + Wrapping(rhs)).0
+}
+fn wrapping_mul<const LIMBS: usize>(lhs: UInt<LIMBS>, rhs: UInt<LIMBS>) -> UInt<LIMBS> {
+    (Wrapping(lhs) * Wrapping(rhs)).0
+}
+fn wrapping_sub<const LIMBS: usize>(lhs: UInt<LIMBS>, rhs: UInt<LIMBS>) -> UInt<LIMBS> {
+    (Wrapping(lhs) - Wrapping(rhs)).0
+}
+
+macro_rules! impl_std_op {
+    ($($op:ident),+) => {
+        $(
+            paste! {
+                impl<const LIMBS: usize> $op for Unsigned<LIMBS> {
+                    type Output = Self;
+
+                    fn [<$op:lower>](self, rhs: Unsigned<LIMBS>) -> Self::Output {
+                        Self::Output {
+                            val: self.val.[<wrapping_ $op:lower>](&rhs.val),
+                        }
+                    }
+                }
+
+                impl<const LIMBS: usize> $op<UInt<LIMBS>> for Unsigned<LIMBS> {
+                    type Output = Self;
+
+                    fn [<$op:lower>](self, rhs: UInt<LIMBS>) -> Self::Output {
+                        Self {
+                            val: self.val.[<wrapping_ $op:lower>](&rhs),
+                        }
+                    }
+                }
+
+                impl<const LIMBS: usize> $op<Unsigned<LIMBS>> for UInt<LIMBS> {
+                    type Output = Unsigned<LIMBS>;
+
+                    fn [<$op:lower>](self, rhs: Self::Output) -> Self::Output {
+                        Self::Output {
+                            val: self.[<wrapping_ $op:lower>](&rhs.val),
+                        }
+                    }
+                }
+
+                impl<const LIMBS: usize> $op<u64> for Unsigned<LIMBS> {
+                    type Output = Self;
+
+                    fn [<$op:lower>](self, rhs: u64) -> Self::Output {
+                        Self {
+                            val: self.val.[<wrapping_ $op:lower>](&UInt::from_u64(rhs)),
+                        }
+                    }
+                }
+
+                impl<const LIMBS: usize> $op<Unsigned<LIMBS>> for u64 {
+                    type Output = Unsigned<LIMBS>;
+
+                    fn [<$op:lower>](self, rhs: Self::Output) -> Self::Output {
+                        Self::Output {
+                            val: UInt::from_u64(self).[<wrapping_ $op:lower>](&rhs.val),
+                        }
+                    }
+                }
+            }
+        )+
+    };
+}
+
+impl_std_op! {
+    Add, Sub, Mul
+}
+
+macro_rules! impl_graph_cipher_op {
+    ($(($op:ident, $op_noun:ident)),+) => {
+        $(
+            paste! {
+                impl<const LIMBS: usize> [<GraphCipher $op>] for Unsigned<LIMBS> {
+                    type Left = Self;
+                    type Right = Self;
+
+                    fn [<graph_cipher_ $op:lower>](
+                        a: FheProgramNode<Cipher<Self::Left>>,
+                        b: FheProgramNode<Cipher<Self::Right>>,
+                    ) -> FheProgramNode<Cipher<Self::Left>> {
+                        with_fhe_ctx(|ctx| {
+                            let n = ctx.[<add_ $op_noun>](a.ids[0], b.ids[0]);
+
+                            FheProgramNode::new(&[n])
+                        })
+                    }
+                }
+
+                impl<const LIMBS: usize> [<GraphCipherPlain $op>] for Unsigned<LIMBS> {
+                    type Left = Self;
+                    type Right = Self;
+
+                    fn [<graph_cipher_plain_ $op:lower>](
+                        a: FheProgramNode<Cipher<Self::Left>>,
+                        b: FheProgramNode<Self::Right>,
+                    ) -> FheProgramNode<Cipher<Self::Left>> {
+                        with_fhe_ctx(|ctx| {
+                            let n = ctx.[<add_ $op_noun _plaintext>](a.ids[0], b.ids[0]);
+
+                            FheProgramNode::new(&[n])
+                        })
+                    }
+                }
+
+                impl<const LIMBS: usize> [<GraphCipherConst $op>] for Unsigned<LIMBS> {
+                    type Left = Self;
+                    type Right = UInt<LIMBS>;
+
+                    fn [<graph_cipher_const_ $op:lower>](
+                        a: FheProgramNode<Cipher<Self::Left>>,
+                        b: UInt<LIMBS>,
+                    ) -> FheProgramNode<Cipher<Self::Left>> {
+                        with_fhe_ctx(|ctx| {
+                            let b = Self::from(b).try_into_plaintext(&ctx.data).unwrap();
+
+                            let lit = ctx.add_plaintext_literal(b.inner);
+                            let [<$op:lower>] = ctx.[<add_ $op_noun _plaintext>](a.ids[0], lit);
+
+                            FheProgramNode::new(&[[<$op:lower>]])
+                        })
+                    }
+                }
+            }
+        )+
+    };
+}
+
+impl_graph_cipher_op! {
+    (Add, addition),
+    (Sub, subtraction),
+    (Mul, multiplication)
+}
+
+impl<const LIMBS: usize> GraphConstCipherSub for Unsigned<LIMBS> {
+    type Left = UInt<LIMBS>;
+    type Right = Self;
+
+    fn graph_const_cipher_sub(
+        a: UInt<LIMBS>,
+        b: FheProgramNode<Cipher<Self::Right>>,
+    ) -> FheProgramNode<Cipher<Self::Right>> {
+        with_fhe_ctx(|ctx| {
+            let a = Self::from(a).try_into_plaintext(&ctx.data).unwrap();
+
+            let lit = ctx.add_plaintext_literal(a.inner);
+            let n = ctx.add_subtraction_plaintext(b.ids[0], lit);
+            let n = ctx.add_negate(n);
+
+            FheProgramNode::new(&[n])
+        })
+    }
+}
+
+impl<const LIMBS: usize> GraphPlainCipherSub for Unsigned<LIMBS> {
+    type Left = Self;
+    type Right = Self;
+
+    fn graph_plain_cipher_sub(
+        a: FheProgramNode<Self::Left>,
+        b: FheProgramNode<Cipher<Self::Right>>,
+    ) -> FheProgramNode<Cipher<Self::Left>> {
+        with_fhe_ctx(|ctx| {
+            let n = ctx.add_subtraction_plaintext(b.ids[0], a.ids[0]);
+            let n = ctx.add_negate(n);
+
+            FheProgramNode::new(&[n])
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_add_non_fhe() {
+        let a = Unsigned256::from(5);
+        let b = Unsigned256::from(10);
+
+        assert_eq!(a + b, 15.into());
+        assert_eq!(a + 10, 15.into());
+        assert_eq!(10 + a, 15.into());
+    }
+
+    #[test]
+    fn can_mul_non_fhe() {
+        let a = Unsigned64::from(5);
+        let b = Unsigned64::from(10);
+
+        assert_eq!(a * b, 50.into());
+        assert_eq!(a * 10, 50.into());
+        assert_eq!(10 * a, 50.into());
+    }
+
+    #[test]
+    fn can_sub_non_fhe() {
+        let a = Unsigned128::from(5);
+        let b = Unsigned128::from(11);
+
+        assert_eq!(b - a, 6.into());
+        assert_eq!(b - 5, 6.into());
+    }
+}
---
+++ sunscreen/tests/unsigned.rs
@@ -0,0 +1,167 @@
+use crypto_bigint::{UInt, U256, U64};
+use paste::paste;
+use proptest::prelude::{prop::num::u64::ANY, prop_assert_eq, proptest, ProptestConfig};
+use sunscreen::{
+    fhe_program,
+    types::{
+        bfv::{Unsigned, Unsigned256},
+        Cipher,
+    },
+    Compiler, FheApplication, FheProgramInput, FheRuntime, PrivateKey, PublicKey, Runtime,
+};
+
+// Darn, Application is no longer thread safe, no lazy init :(
+// luckily proptest supports something like an expensive setup operation
+// TODO tests for more than just 256?
+
+macro_rules! fhe_program {
+    ($(($op:ident, $binop:tt, $ty:ident)),+) => {
+        $(
+            paste! {
+                #[fhe_program(scheme = "bfv")]
+                fn $op(a: Cipher<$ty>, b: Cipher<$ty>) -> Cipher<$ty> {
+                    a $binop b
+                }
+                #[fhe_program(scheme = "bfv")]
+                fn [<$op _plain>](a: Cipher<$ty>, b: $ty) -> Cipher<$ty> {
+                    a $binop b
+                }
+            }
+        )+
+     };
+}
+
+fhe_program! {
+    (add, +, Unsigned256),
+    (sub, -, Unsigned256),
+    (mul, *, Unsigned256)
+}
+
+struct FheApp {
+    app: FheApplication,
+    rt: FheRuntime,
+    pk: PublicKey,
+    sk: PrivateKey,
+}
+impl FheApp {
+    fn new() -> Self {
+        let app: FheApplication = Compiler::new()
+            .fhe_program(add)
+            .fhe_program(add_plain)
+            .fhe_program(sub)
+            .fhe_program(sub_plain)
+            .fhe_program(mul)
+            .fhe_program(mul_plain)
+            .compile()
+            .unwrap();
+        let rt: FheRuntime = Runtime::new_fhe(app.params()).unwrap();
+        let (pk, sk) = rt.generate_keys().unwrap();
+        Self { app, rt, pk, sk }
+    }
+}
+
+// We could use a single outer #[test] function and just have a macro to
+// populate the inside with multiple proptest! calls. This would result in just a
+// single FheApp instantiation, which might speed things up.
+macro_rules! op_proptest {
+    ($($op:ident),+) => {
+        $(
+            paste! {
+                #[test]
+                fn [<$op _fhe_proptest>]() {
+                    let FheApp { app, rt, pk, sk } = FheApp::new();
+
+                    proptest!(ProptestConfig::with_cases(20), |(lhs in [ANY; 4], rhs in [ANY; 4])| {
+
+                        // Test both operands as ciphertexts
+                        let a = U256::from_words(lhs);
+                        let a_c = rt.encrypt(Unsigned256::from(a), &pk).unwrap();
+                        let b = U256::from_words(rhs);
+                        let b_c = rt.encrypt(Unsigned256::from(b), &pk).unwrap();
+                        let args: Vec<FheProgramInput> = vec![a_c.clone().into(), b_c.clone().into()];
+
+                        let result = rt
+                            .run(app.get_fhe_program($op).unwrap(), args, &pk)
+                            .unwrap();
+
+                        let c: Unsigned256 = rt.decrypt(&result[0], &sk).unwrap();
+
+                        prop_assert_eq!(a.[<wrapping_ $op>](&b), c.into());
+
+                        // Test mixed ciphertexts and plaintexts
+                        let args_mixed: Vec<FheProgramInput> = vec![a_c.into(), Unsigned256::from(b).into()];
+                        let result_mixed = rt
+                            .run(app.get_fhe_program([<$op _plain>]).unwrap(), args_mixed, &pk)
+                            .unwrap();
+
+                        let c_mixed: Unsigned256 = rt.decrypt(&result_mixed[0], &sk).unwrap();
+
+                        prop_assert_eq!(a.[<wrapping_ $op>](&b), c_mixed.into());
+                    });
+                }
+            }
+        )+
+     };
+}
+
+op_proptest! {
+    add,
+    sub,
+    mul
+}
+
+fn run_with<const L: usize, O1, O2, F>(a: UInt<L>, b: UInt<L>, op: F, fhe_op: O1, fhe_op_plain: O2)
+where
+    O1: AsRef<str>,
+    O2: AsRef<str>,
+    F: Fn(&UInt<L>, &UInt<L>) -> UInt<L>,
+{
+    let FheApp { app, rt, pk, sk } = FheApp::new();
+    let a_u = Unsigned::from(a);
+    let b_u = Unsigned::from(b);
+
+    let a_c = rt.encrypt(a_u, &pk).unwrap();
+    let b_c = rt.encrypt(b_u, &pk).unwrap();
+    let args: Vec<FheProgramInput> = vec![a_c.clone().into(), b_c.into()];
+
+    let result = rt
+        .run(app.get_fhe_program(fhe_op).unwrap(), args, &pk)
+        .unwrap();
+
+    let c: Unsigned<L> = rt.decrypt(&result[0], &sk).unwrap();
+
+    assert_eq!(op(&a, &b), c.into());
+
+    // Same test but subtracting plaintext
+    let args_mixed: Vec<FheProgramInput> = vec![a_c.into(), b_u.into()];
+    let result_mixed = rt
+        .run(app.get_fhe_program(fhe_op_plain).unwrap(), args_mixed, &pk)
+        .unwrap();
+
+    let c_mixed: Unsigned<L> = rt.decrypt(&result_mixed[0], &sk).unwrap();
+
+    assert_eq!(op(&a, &b), c_mixed.into());
+}
+
+#[test]
+fn underflow_wraps_properly() {
+    // U256
+    run_with(U256::ZERO, U256::ONE, U256::wrapping_sub, sub, sub_plain);
+    // U64
+    run_with(U64::ZERO, U64::ONE, U64::wrapping_sub, sub, sub_plain);
+}
+
+#[test]
+fn overflow_wraps_properly() {
+    // U256
+    run_with(U256::MAX, U256::ONE, U256::wrapping_add, add, add_plain);
+    // U64
+    run_with(U64::MAX, U64::ONE, U64::wrapping_add, add, add_plain);
+}
+
+#[test]
+fn carry_at_limb_boundary() {
+    let a = U256::from_words([0, u64::MAX, 0, 0]);
+    let b = U256::from_words([0, 1, 0, 0]);
+    run_with(a, b, U256::wrapping_add, add, add_plain);
+}

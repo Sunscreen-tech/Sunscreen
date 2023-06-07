c6c26079838c3d4efe430917d19f08e9a5d7ffa2
Sam Tay <samctay@pm.me>
2023-06-06 18:07:43 -0700

    Fix clippy 1.70 (#249)
    And duplicate warnings for good measure

--- benchmarks/bfv_zkp/src/main.rs
+++ benchmarks/bfv_zkp/src/main.rs
@@ -1,8 +1,7 @@
 mod bfv;
 mod poly_ring;
 
-///! STOP. DO NOT USE THIS CODE FOR PRODUCTION.
-///! Security is a non-goal for this library. In fact, this library is known
-///! to be insecure.
-
+/// STOP. DO NOT USE THIS CODE FOR PRODUCTION.
+/// Security is a non-goal for this library. In fact, this library is known
+/// to be insecure.
 fn main() {}

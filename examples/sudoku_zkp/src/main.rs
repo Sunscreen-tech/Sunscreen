bbce98adae39c9ab649f863477dec58c583b9bf5
Sam Tay <samctay@pm.me>
2023-07-05 16:26:32 -0500

    Change sudoku to use pub over const

--- examples/sudoku_zkp/src/main.rs
+++ examples/sudoku_zkp/src/main.rs
@@ -4,54 +4,9 @@ use sunscreen::{
     zkp_program, zkp_var, BackendField, Compiler, Error, ZkpProgramInput, ZkpRuntime,
 };
 
-fn main() -> Result<(), Error> {
-    let app = Compiler::new()
-        .zkp_backend::<BulletproofsBackend>()
-        .zkp_program(sudoku_proof)
-        .compile()?;
-
-    let prog = app.get_zkp_program(sudoku_proof).unwrap();
-
-    let runtime = ZkpRuntime::new(&BulletproofsBackend::new())?;
-
-    let ex_puzzle = [
-        [0, 7, 0, 0, 2, 0, 0, 4, 6],
-        [0, 6, 0, 0, 0, 0, 8, 9, 0],
-        [2, 0, 0, 8, 0, 0, 7, 1, 5],
-        [0, 8, 4, 0, 9, 7, 0, 0, 0],
-        [7, 1, 0, 0, 0, 0, 0, 5, 9],
-        [0, 0, 0, 1, 3, 0, 4, 8, 0],
-        [6, 9, 7, 0, 0, 2, 0, 0, 8],
-        [0, 5, 8, 0, 0, 0, 0, 6, 0],
-        [4, 3, 0, 0, 8, 0, 0, 7, 0],
-    ];
-
-    let ex_sol = [
-        [8, 7, 5, 9, 2, 1, 3, 4, 6],
-        [3, 6, 1, 7, 5, 4, 8, 9, 2],
-        [2, 4, 9, 8, 6, 3, 7, 1, 5],
-        [5, 8, 4, 6, 9, 7, 1, 2, 3],
-        [7, 1, 3, 2, 4, 8, 6, 5, 9],
-        [9, 2, 6, 1, 3, 5, 4, 8, 7],
-        [6, 9, 7, 4, 1, 2, 5, 3, 8],
-        [1, 5, 8, 3, 7, 9, 2, 6, 4],
-        [4, 3, 2, 5, 8, 6, 9, 7, 1],
-    ];
-
-    let board: Vec<ZkpProgramInput> = vec![ex_sol.map(|a| a.map(BulletproofsField::from)).into()];
-
-    let cons: Vec<ZkpProgramInput> = vec![ex_puzzle.map(|a| a.map(BulletproofsField::from)).into()];
-
-    let proof = runtime.prove(prog, cons.clone(), vec![], board)?;
-
-    runtime.verify(prog, &proof, cons, vec![])?;
-
-    Ok(())
-}
-
 #[zkp_program]
 fn sudoku_proof<F: BackendField>(
-    #[constant] constraints: [[NativeField<F>; 9]; 9],
+    #[public] constraints: [[NativeField<F>; 9]; 9],
     board: [[NativeField<F>; 9]; 9],
 ) {
     let zero = zkp_var!(0);
@@ -106,6 +61,51 @@ fn sudoku_proof<F: BackendField>(
     }
 }
 
+fn main() -> Result<(), Error> {
+    let app = Compiler::new()
+        .zkp_backend::<BulletproofsBackend>()
+        .zkp_program(sudoku_proof)
+        .compile()?;
+
+    let prog = app.get_zkp_program(sudoku_proof).unwrap();
+
+    let runtime = ZkpRuntime::new(&BulletproofsBackend::new())?;
+
+    let ex_puzzle = [
+        [0, 7, 0, 0, 2, 0, 0, 4, 6],
+        [0, 6, 0, 0, 0, 0, 8, 9, 0],
+        [2, 0, 0, 8, 0, 0, 7, 1, 5],
+        [0, 8, 4, 0, 9, 7, 0, 0, 0],
+        [7, 1, 0, 0, 0, 0, 0, 5, 9],
+        [0, 0, 0, 1, 3, 0, 4, 8, 0],
+        [6, 9, 7, 0, 0, 2, 0, 0, 8],
+        [0, 5, 8, 0, 0, 0, 0, 6, 0],
+        [4, 3, 0, 0, 8, 0, 0, 7, 0],
+    ];
+
+    let ex_sol = [
+        [8, 7, 5, 9, 2, 1, 3, 4, 6],
+        [3, 6, 1, 7, 5, 4, 8, 9, 2],
+        [2, 4, 9, 8, 6, 3, 7, 1, 5],
+        [5, 8, 4, 6, 9, 7, 1, 2, 3],
+        [7, 1, 3, 2, 4, 8, 6, 5, 9],
+        [9, 2, 6, 1, 3, 5, 4, 8, 7],
+        [6, 9, 7, 4, 1, 2, 5, 3, 8],
+        [1, 5, 8, 3, 7, 9, 2, 6, 4],
+        [4, 3, 2, 5, 8, 6, 9, 7, 1],
+    ];
+
+    let board: Vec<ZkpProgramInput> = vec![ex_sol.map(|a| a.map(BulletproofsField::from)).into()];
+
+    let pubs: Vec<ZkpProgramInput> = vec![ex_puzzle.map(|a| a.map(BulletproofsField::from)).into()];
+
+    let proof = runtime.prove(prog, vec![], pubs.clone(), board)?;
+
+    runtime.verify(prog, &proof, vec![], pubs)?;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -149,12 +149,12 @@ mod tests {
         let board: Vec<ZkpProgramInput> =
             vec![ex_sol.map(|a| a.map(BulletproofsField::from)).into()];
 
-        let cons: Vec<ZkpProgramInput> =
+        let pubs: Vec<ZkpProgramInput> =
             vec![ex_puzzle.map(|a| a.map(BulletproofsField::from)).into()];
 
-        let proof = runtime.prove(prog, cons.clone(), vec![], board).unwrap();
+        let proof = runtime.prove(prog, vec![], pubs, board).unwrap();
 
-        let verify = runtime.verify(prog, &proof, cons, vec![]);
+        let verify = runtime.verify(prog, &proof, vec![], pubs);
 
         assert!(verify.is_ok());
     }
@@ -198,10 +198,10 @@ mod tests {
         let board: Vec<ZkpProgramInput> =
             vec![ex_sol.map(|a| a.map(BulletproofsField::from)).into()];
 
-        let cons: Vec<ZkpProgramInput> =
+        let pubs: Vec<ZkpProgramInput> =
             vec![ex_puzzle.map(|a| a.map(BulletproofsField::from)).into()];
 
-        let proof = runtime.prove(prog, cons, vec![], board);
+        let proof = runtime.prove(prog, vec![], pubs, board);
 
         assert!(proof.is_err());
     }
@@ -235,10 +235,10 @@ mod tests {
         let board: Vec<ZkpProgramInput> =
             vec![ex_sol.map(|a| a.map(BulletproofsField::from)).into()];
 
-        let cons: Vec<ZkpProgramInput> =
+        let pubs: Vec<ZkpProgramInput> =
             vec![ex_puzzle.map(|a| a.map(BulletproofsField::from)).into()];
 
-        let proof = runtime.prove(prog, cons, vec![], board);
+        let proof = runtime.prove(prog, vec![], pubs, board);
 
         assert!(proof.is_err());
     }

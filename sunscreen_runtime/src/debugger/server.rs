e5916cbde0705041b381a27c1aee2da6c11b1557
Matthew Liu <matthew_liu@berkeley.edu>
2023-07-17 11:20:38 -0700

    added support for cors so frontend can now grab data from backend. currently it only grabs a string that allows the frontend to construct a graph

--- sunscreen_runtime/src/debugger/server.rs
+++ sunscreen_runtime/src/debugger/server.rs
@@ -1,8 +1,10 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Handler};
 use reqwest;
+use actix_cors::Cors;
 use serde::{Deserialize, Serialize};
-use serde_json::{to_string, to_string_pretty, Value, Error};
+use serde_json::{to_string, to_string_pretty, Value, Error, json};
 use std::collections::HashMap;
+use std::ops::IndexMut;
 use std::sync::{Mutex, MutexGuard, OnceLock};
 
 
@@ -23,8 +25,14 @@ pub async fn start_web_server() -> std::io::Result<()> {
     match reqwest::get(url).await {
         Ok(_response) => Ok(()),
         Err(_e) => {
-            HttpServer::new(|| {
+
+            HttpServer::new(move || {
+                let cors = Cors::default()
+                .allow_any_origin()
+                .allowed_methods(vec!["GET"]);
+    
                 App::new()
+                    .wrap(cors)
                     .service(get_graph_data) 
             })
                 .bind(("127.0.0.1", 8080))?
@@ -46,10 +54,10 @@ async fn get_graph_data(session: web::Path<String>) -> impl Responder {
         let curr_session = sessions.get(session.as_str()).unwrap().unwrap_bfv_session();
         let graph_string = serde_json::to_string_pretty(&curr_session.graph.graph);
 
-        //let frontend_graph = process_graph_json(&graph_string.unwrap());
-        //HttpResponse::Ok().body(frontend_graph.unwrap().to_owned())
+        let frontend_graph = process_graph_json(&graph_string.unwrap());
+        HttpResponse::Ok().body(frontend_graph.unwrap().to_owned())
 
-        HttpResponse::Ok().body(graph_string.unwrap().to_owned())
+        //HttpResponse::Ok().body(graph_string.unwrap().to_owned())
     } else {
         HttpResponse::NotFound().body("Session not found.".to_owned())
     }
@@ -66,6 +74,44 @@ async fn get_node_data(session: String, runtime: FheRuntime, priv_key: &PrivateK
 */
 
  
-fn process_graph_json(json_str: &str) -> String {
-    "".to_owned()
+#[derive(Debug, Deserialize)]
+struct GraphFormat {
+    nodes: Vec<Value>,
+    edges: Vec<Vec<Value>>,
+}
+
+fn process_graph_json(json_str: &str) -> Result<String, serde_json::Error> {
+    let input_json: serde_json::Value = serde_json::from_str(json_str).unwrap();
+
+    let mut new_nodes = Vec::new();
+    let mut new_edges = Vec::new();
+
+    if let Some(nodes) = input_json.get("nodes") {
+        for (index, _) in nodes.as_array().unwrap().iter().enumerate() {
+            new_nodes.push(json!({
+                "type": "Type",
+                "title": "blank",
+                "id": index, 
+            }));
+        }
+    }
+
+    if let Some(edges) = input_json.get("edges") {
+        for edge in edges.as_array().unwrap() {
+            new_edges.push(json!({
+                "arrowhead": "normal",
+                "directed": true, 
+                "target": edge[1].as_i64().unwrap(),
+                "source": edge[0].as_i64().unwrap(),
+            }));
+        }
+    }
+
+    let output = json!({
+        "nodes": new_nodes,
+        "edges": new_edges
+    });
+
+    Ok(output.to_string())
+
 }

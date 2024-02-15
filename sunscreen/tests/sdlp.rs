b2c34cb4d1d99fef36f1d312a53a831e6444ff7f
Sam Tay <samctay@pm.me>
2024-02-15 15:38:38 -0500

    Demonstrate decryption statement proof error

--- sunscreen/tests/sdlp.rs
+++ sunscreen/tests/sdlp.rs
@@ -2,7 +2,11 @@
 mod sdlp_tests {
     use lazy_static::lazy_static;
     use logproof::rings::SealQ128_1024;
-    use sunscreen::types::bfv::{Fractional, Signed, Unsigned64};
+    use sunscreen::{
+        types::bfv::{Fractional, Signed, Unsigned64},
+        FheProgramFnExt,
+    };
+    use sunscreen_compiler_macros::fhe_program;
     use sunscreen_fhe_program::SchemeType;
 
     use sunscreen_runtime::{FheRuntime, LogProofBuilder, Params};
@@ -65,15 +69,28 @@ mod sdlp_tests {
     }
 
     #[test]
+    #[ignore = "requires seal fix"]
     fn prove_refreshing_existing_ciphertext() {
         let rt = FheRuntime::new(&TEST_PARAMS).unwrap();
         let (public_key, private_key) = rt.generate_keys().unwrap();
         let mut logproof_builder = LogProofBuilder::new(&rt);
 
-        let existing_ct = rt.encrypt(Signed::from(100), &public_key).unwrap();
+        let initial_ct = rt.encrypt(Signed::from(100), &public_key).unwrap();
 
-        let (_existing_val, msg) = logproof_builder
-            .decrypt_returning_msg::<Signed>(&existing_ct, &private_key)
+        #[fhe_program(scheme = "bfv")]
+        fn double(x: sunscreen::types::Cipher<Signed>) -> sunscreen::types::Cipher<Signed> {
+            x + x
+        }
+
+        let double_compiled = double.compile().unwrap();
+
+        let computed_ct = rt
+            .run(&double_compiled, vec![initial_ct], &public_key)
+            .unwrap()
+            .remove(0);
+
+        let (_, msg) = logproof_builder
+            .decrypt_returning_msg::<Signed>(&computed_ct, &private_key)
             .unwrap();
         let _refreshed_ct = logproof_builder.encrypt_msg(&msg, &public_key).unwrap();
 

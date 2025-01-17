c9ac4644f7840b989d2850ddc946c0ba5e9d14dd
rickwebiii <rick.weber.iii@gmail.com>
2025-01-17 15:36:07 -0800

    wtf

--- seal_fhe/build.rs
+++ seal_fhe/build.rs
@@ -1,4 +1,3 @@
-use bindgen::CargoCallbacks;
 use cmake::Config;
 use emsdk::Config as EmConfig;
 
@@ -154,7 +153,7 @@ fn main() {
     let builder = builder
         .detect_include_paths(true)
         .header("bindgen_wrapper.h")
-        .parse_callbacks(Box::new(CargoCallbacks::new()))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .allowlist_function("BatchEncoder_.*")
         .allowlist_function("Ciphertext_.*")
         .allowlist_function("CKKSEncoder_.*")
